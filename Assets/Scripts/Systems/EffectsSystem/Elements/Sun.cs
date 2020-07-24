e47dabfcf5106484556ece5a6f683263e9dce46a
Pokoi <jesusferminvillar@gmail.com>
2020-07-25 01:15:52 +0200

    Temperature system modified

--- Assets/Scripts/Systems/EffectsSystem/Elements/Sun.cs
+++ Assets/Scripts/Systems/EffectsSystem/Elements/Sun.cs
@@ -1,5 +1,6 @@
 ﻿
 using UnityEngine;
+using System;
 
 namespace Garden
 {
@@ -7,10 +8,41 @@ namespace Garden
     public class Sun : MonoBehaviour
     {
         [SerializeField]
-        private float currentSun = 5f;
-        private float startingSun;
+        private float startingSun = 5f;
+        private float currentSun;
         public float GetCurrentSun => currentSun;
         private SunEffect sunEffect;
+
+        /// <summary>
+        /// A struct with the data about the season heat changes
+        /// </summary>
+        [Serializable]
+        private struct SeasonHeatModifiers 
+        {
+            public float summer;
+            public float fall   ;
+            public float winter ;
+            public float spring ;
+        }
+
+        /// <summary>
+        /// A struct with the data about the day-cycle heat changes
+        /// </summary>
+        [Serializable]
+        private struct DayHeatModifiers
+        {
+            public float dawn;      
+            public float midday;
+            public float sunset;
+            public float midnight;
+        }
+
+        [SerializeField] SeasonHeatModifiers seasonHeatModifiers;
+        [SerializeField] DayHeatModifiers dayHeatModifiers;
+
+
+        string currentDayState;
+        string currentSeason ;
 
         //[SerializeField] Delegate [] delegates;
 
@@ -19,55 +51,53 @@ namespace Garden
 
             //Add the effect to the effectsystem
             SceneSystem.Instance.GetEffectsSystem.AddEffect(sunEffect);
-
-            startingSun = currentSun;
+                       
         }
 
         //The delegate call this funcion when day night state is changed
-        public void ChangeSunIntensity(string state){
-
-            switch(state){
-                case "dawn":
-                currentSun = startingSun;
-                break;
-
-                case "midday":
-                currentSun*=2;
-                break;
-
-                case "sunset":
-                currentSun/=3;
-                break;
-
-                case "midnight":
-                currentSun = 0f;
-                break;
-            }
+        public void ChangeSunIntensity(string state)
+        {
+            currentDayState = state;
+            UpdateTemperature();
         }
 
-        public void SeasonChanged(string season){
-            
-            switch(season){
-                case "spring":
-                currentSun += 2;
-                break;
+        public void SeasonChanged(string season)
+        {
+            currentSeason = season;
+            UpdateTemperature();
+        }
 
-                case "summer":
-                currentSun*=2;
-                break;
+        /// <summary>
+        /// Updates the temperature value based on all the conditions
+        /// </summary>
+        void UpdateTemperature()
+        {
+            float seasonModifier = 1f;
+            float dayModifier = 1f;
 
-                case "autumn":
-                currentSun= startingSun;
-                break;
-
-                case "winter":
-                currentSun -= 10f;
-                break;
+            switch (currentDayState)
+            {
+                case "dawn":     dayModifier = dayHeatModifiers.dawn;     break;
+                case "midday":   dayModifier = dayHeatModifiers.midday;   break;
+                case "sunset":   dayModifier = dayHeatModifiers.sunset;   break;
+                case "midnight": dayModifier = dayHeatModifiers.midnight; break;            
             }
+
+            switch (currentSeason)
+            {
+                case "summer": seasonModifier = seasonHeatModifiers.summer; break;
+                case "fall": seasonModifier = seasonHeatModifiers.fall; break;
+                case "winter": seasonModifier = seasonHeatModifiers.winter; break;
+                case "spring": seasonModifier = seasonHeatModifiers.spring; break;
+            }
+
+            currentSun = startingSun * seasonModifier * dayModifier;
+
+            Debug.Log("Potencia del sol: " + currentSun + "ºC al " + currentDayState + " en " + currentSeason);
         }
 
         void Update(){
-            Debug.Log("Potencia del sol: " + currentSun);
+           
         }
         
     }
