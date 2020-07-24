0b5b3424076e760a57e22c5ba1b07de089a8ea95
Pokoi <jesusferminvillar@gmail.com>
2020-07-24 19:15:22 +0200

    System base clase added

---
+++ Assets/Scripts/Base/System.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Garden
+{
+    public abstract class System 
+    {
+        bool active;
+
+       public abstract void Update();
+
+       public bool is_active => active;
+    }
+
+}
