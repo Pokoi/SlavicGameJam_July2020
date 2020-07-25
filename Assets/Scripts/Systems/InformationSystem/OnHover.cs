c4ef4e491b9b8aae17b5ed5edfc91b27e3692123
srgssj <ssjsrgx@gmail.com>
2020-07-25 13:26:28 +0200

    Mouse cursor change and click detection

--- Assets/Scripts/Systems/InformationSystem/OnHover.cs
+++ Assets/Scripts/Systems/InformationSystem/OnHover.cs
@@ -7,7 +7,13 @@ namespace Garden
     public class OnHover : MonoBehaviour
     {
         public string elementName;
+
+        [TextArea]
+        public string textToShow;
+
         [SerializeField] Delegate[] delegates;
+
+        private Component currComponent = null;
 
         void Start(){
             
@@ -15,19 +21,74 @@ namespace Garden
 
         void OnMouseOver()
         {
-            foreach (Delegate sender in delegates)
-            {
-                sender.Run(elementName);
-            }
+                //Information popup
+                Delegate.InformationElement hElement;
+                hElement.elementName = elementName;
+                hElement.mouseOver = true;
+                hElement.textToShow = textToShow;
+                hElement.component = null;
+
+                delegates[0].Run(hElement);
+
+                ///////////
+            
+                //Cursor delegate
+                
+                Delegate.InformationElement hCursor;
+                hCursor.elementName = elementName;
+                hCursor.mouseOver = true;
+                hCursor.textToShow = "";
+
+                if(currComponent==null){
+                if(TryGetComponent(out WateringCan wCan)){
+                    currComponent = wCan;
+                }
+                else if(TryGetComponent(out Plant plant))
+                    currComponent = null;
+                    }
+
+                hCursor.component = currComponent;
+                delegates[1].Run(hCursor);
+            
         }
         void OnMouseExit()
         {
-            foreach (Delegate sender in delegates)
-            {
-                sender.Run(elementName);
-            }
+            
+                //Information popout
+                Delegate.InformationElement hElement;
+                hElement.elementName = elementName;
+                hElement.mouseOver = false;
+                hElement.textToShow = "";
+                hElement.component = null;
+
+                delegates[0].Run(hElement);
+
+                ///////////
+
+                //Cursor delegate
+
+                Delegate.InformationElement hCursor;
+                hCursor.elementName = elementName;
+                hCursor.mouseOver = false;
+                hCursor.textToShow = "";
+
+              
+               if(currComponent==null){
+                if(TryGetComponent(out WateringCan wCan)){
+                    currComponent = wCan;
+                }
+                else if(TryGetComponent(out Plant plant))
+                    currComponent = null;
+               }
+
+                hCursor.component = currComponent;
+                delegates[1].Run(hCursor);
+
         }
 
+        void OnMouseDown(){
+           delegates[2].Run(currComponent);
+        }
 
     }
 }
