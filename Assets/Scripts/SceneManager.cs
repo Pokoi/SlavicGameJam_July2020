82ebf6c015d038e87b8692e08ddd058dfdd62671
srgssj <ssjsrgx@gmail.com>
2020-07-26 16:30:11 +0200

    Main menu and fixes

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace Garden
+{
+    public class SceneManager : MonoBehaviour
+    {
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex+1);
+            }
+        }
+    }
+}
